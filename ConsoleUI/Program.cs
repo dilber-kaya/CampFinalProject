1d319616db2129a7a4f3e826d42afe24a7e99bc3
Dilber <Dilber@DESKTOP-VHNG40S>
2025-01-20 16:23:58 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+//IProductDal (interface) InMemoryProductDal' ın referansını tutar
+
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Dal: Data Access Layer (.NET) or Dao: Data Access Object (Java)
+    public interface IProductDal:IEntityRepository<Product>
+    {
+    }
+
+    //interface metodları default public dir.
+}
