b98cca074232a3b065c50ef541a18a8fcef66dad
dilber-kaya <dilber.kayaa96@gmail.com>
2025-03-21 23:44:49 +0300

    Result Configuration

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    //static : newlenmeyen, uygulama boyunca tek instace olur
+    public static class Messages
+    {
+        //publicler (değişken isimleri) büyük harfle (PascalCase) yazılır
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+            //data ve mesaj verir
+        }
+
+        public ErrorDataResult(T data) : base(data, false)
+        {
+            //sadece data verir
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+            //sadece mesaj verir
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+            //hiçbir şey vermez
+        }
+    }
+}
